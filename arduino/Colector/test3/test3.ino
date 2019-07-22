ee2ed864d62468dc5e2cb6b8dd2f663a6573f721
Valahul123 <34607804+Valahul123@users.noreply.github.com>
2019-07-22 13:26:46 +0300

    Add files via upload

---
+++ arduino/Colector/test3/test3.ino
@@ -0,0 +1,109 @@
+#include <Wire.h>
+#define USE_ARDUINO_INTERRUPTS true
+#include <PulseSensorPlayground.h>
+
+//  Variables
+const int PulseWire = 2;       // PulseSensor PURPLE WIRE connected to ANALOG PIN 0         // The on-board Arduino LED, close to PIN 13.
+int Threshold = 550;           // Determine which Signal to "count as a beat" and which to ignore.
+int umd;
+int tempBody;
+int temp;                      // Use the "Gettting Started Project" to fine-tune Threshold Value beyond default setting.
+int myBPM;                      // Otherwise leave the default "550" value.
+int pulse = 0;
+PulseSensorPlayground pulseSensor;  // Creates an instance of the PulseSensorPlayground object called "pulseSensor"
+
+
+void setup() {
+
+  Serial.begin(9600);          // For Serial Monitor
+  Wire.begin(7); // join i2c bus with address #7
+  Wire.onRequest(requestEvent); // register event
+  // Configure the PulseSensor object, by assigning our variables to it.
+  pulseSensor.analogInput(PulseWire);
+  pulseSensor.setThreshold(Threshold);
+ 
+
+  // Double-check the "pulseSensor" object was created and "began" seeing a signal.
+  if (pulseSensor.begin()) {
+    Serial.println("We created a pulseSensor Object !");  //This prints one time at Arduino power-up,  or on Arduino reset.
+  }
+}
+
+
+
+void loop() {
+
+  myBPM = pulseSensor.getBeatsPerMinute();  // Calls function on our pulseSensor object that returns BPM as an "int".
+
+
+   Serial.println(temp);
+  //Serial.println(umd);                                             // "myBPM" hold this BPM value now.
+  Serial.println(tempBody);
+  
+
+  if (pulseSensor.sawStartOfBeat()) {            // Constantly test to see if "a beat happened".
+    pulse = myBPM;
+    //Serial.println("♥  A HeartBeat Happened ! "); // If test is "true", print a message "a heartbeat happened".
+    //Serial.print("BPM: ");                        // Print phrase "BPM: "
+    //Serial.println(myBPM);                        // Print the value inside of myBPM.
+  }
+
+  delay(20);                    // considered best practice in a simple sketch.
+
+}
+
+void requestEvent() {
+  Serial.println("OK");
+  inv(pulse);
+  Wire.write("*");
+  temp = readTempInCelsius(10, 0);
+  umd = readUmd(1);
+  tempBody = readTempInCelsius(10, 3);
+
+
+  inv(temp);
+  Wire.write("*");
+  inv(umd);
+  Wire.write("*");
+  inv(tempBody);
+  pulse = 0;
+}
+
+float readTempInCelsius(int count, int pin) {
+  float temperaturaMediata = 0;
+  float sumaTemperatura = 0;
+  for (int i = 0; i < count; i++) {
+    int reading = analogRead(pin);
+    float voltage = reading * 5.0;
+    voltage /= 1024.0;
+    float temperatureCelsius = (voltage - 0.5) * 100 ;
+    sumaTemperatura = sumaTemperatura + temperatureCelsius;
+  }
+  return sumaTemperatura / (float)count;
+}
+
+int readUmd(int pin) {
+  int umd = analogRead(pin);
+  while (umd > 1000) {
+    umd--;
+  }
+  return umd / 10;
+
+}
+
+void inv(int n) {
+  int inv = 0, i = 0;
+  char c[3];
+  while (n) {
+    inv = n % 10 + (int)'0';
+    n /= 10;
+    c[i] = inv;
+    i++;
+  }
+  while (i >= 0) {
+    Wire.write(c[i]);
+    i--;
+
+  }
+}
+
